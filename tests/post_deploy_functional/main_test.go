c50165c86f414cad177a7594bc9e7f35f65ab9f2
oscar-hernandez-nttd <335457@nttdata.com>
2026-07-20 10:38:53 -0600

    fix: rename copy-pasted test entrypoint functions
    Both post_deploy_functional and post_deploy_functional_readonly used
    TestSecretsManagerModule, a leftover name from a SecretsManager
    scaffold this module was copied from. Rename to
    TestFirehoseDeliveryStreamModule and
    TestFirehoseDeliveryStreamModuleReadOnly to match the module under
    test.
    
    Addresses PR review feedback:
    https://github.com/launchbynttdata/tf-aws-module_primitive-firehose_delivery_stream/pull/3#pullrequestreview-4725722778

--- tests/post_deploy_functional/main_test.go
+++ tests/post_deploy_functional/main_test.go
@@ -25,7 +25,7 @@ const (
 	infraTFVarFileNameDefault        = "test.tfvars"
 )
 
-func TestSecretsManagerModule(t *testing.T) {
+func TestFirehoseDeliveryStreamModule(t *testing.T) {
 
 	ctx := types.CreateTestContextBuilder().
 		SetTestConfig(&testimpl.ThisTFModuleConfig{}).
--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -25,7 +25,7 @@ const (
 	infraTFVarFileNameDefault        = "test.tfvars"
 )
 
-func TestSecretsManagerModule(t *testing.T) {
+func TestFirehoseDeliveryStreamModuleReadOnly(t *testing.T) {
 
 	ctx := types.CreateTestContextBuilder().
 		SetTestConfig(&testimpl.ThisTFModuleConfig{}).
